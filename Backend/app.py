2a0235ef03425d24f496f2ec5d450d44c68f542f
MrTaooo <95959934+MrTaooo@users.noreply.github.com>
2023-09-18 12:31:51 +0800

    Set up for apply role

--- Backend/app.py
+++ Backend/app.py
@@ -3,8 +3,59 @@ from flask_cors import CORS
 
 import os, sys
 
+import requests
+from invokes import invoke_http
+
 # -------------- Import for Notification Service (START) --------------
 import amqp_setup
 import pika
 import json
 # --------------- Import for Notification Service (End) ---------------
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/apply_role", methods=['POST'])
+def apply_role():
+    # Simple check of input format and data of the request are JSON
+    if request.is_json:
+        try:
+            applicantDetails = request.get_json()
+            print("\nReceived an applicationDetails in JSON:", applicantDetails)
+
+            # do the actual work
+            # 1. Send applicationDetails
+            result = processApplication(applicantDetails)
+            print('\n------------------------')
+            print('\nresult: ', result)
+            return jsonify(result), result["code"]
+
+        except Exception as e:
+            # Unexpected error in code
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            ex_str = str(e) + " at " + str(exc_type) + ": " + fname + ": line " + str(exc_tb.tb_lineno)
+            print(ex_str)
+
+            return jsonify({
+                "code": 500,
+                "message": "app.py internal error: " + ex_str
+            }), 500
+
+    # if reached here, not a JSON request.
+    return jsonify({
+        "code": 400,
+        "message": "Invalid JSON input: " + str(request.get_data())
+    }), 400
+
+# Execute this program if it is run as a main script (not by 'import')
+if __name__ == "__main__":
+    print("This is flask " + os.path.basename(__file__) + " for All-in-one Skill-based Role Portal (SBRP)")
+    app.run(host="0.0.0.0", port=5100, debug=True)
+    # Notes for the parameters: 
+    # - debug=True will reload the program automatically if a change is detected;
+    #   -- it in fact starts two instances of the same flask program, and uses one of the instances to monitor the program changes;
+    # - host="0.0.0.0" allows the flask program to accept requests sent from any IP/host (in addition to localhost),
+    #   -- i.e., it gives permissions to hosts with any IP to access the flask program,
+    #   -- as long as the hosts can already reach the machine running the flask program along the network;
+    #   -- it doesn't mean to use http://0.0.0.0 to access the flask program.
