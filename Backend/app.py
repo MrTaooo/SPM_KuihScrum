037cd1029abfd87ca6d1b41fabbc893ed37e8374
Shiva <60564426+flunkedout@users.noreply.github.com>
2023-09-18 19:56:02 +0800

    Table class, Vue mount
    Added in table classes for Role, Job_Listing.
    
    Added in Vue.js get joblistings, roles

--- Backend/app.py
+++ Backend/app.py
@@ -23,6 +23,88 @@ CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:password@localhost/spm_kuih'  # Replace with your MySQL credentials
 db = SQLAlchemy(app)
 
+# Model Class: Role
+class Role(db.Model):
+    __tablename__ = 'Role'
+
+    Role_Name = db.Column(db.String(20), primary_key=True)
+    Role_Desc = db.Column(db.Longtext, nullable=False)
+
+    def __init__(self, Role_Name, Role_Desc):
+        self.Role_Name = Role_Name
+        self.Role_Desc = Role_Desc
+
+    def json(self):
+        return {
+                "Role_Name": self.Role_Name, 
+                "Role_Desc": self.Role_Desc
+        }
+    
+# Model Class: Job_Listing
+class JobListing(db.Model):
+    __tablename__ = 'Job_Listing'
+
+    JobList_ID = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    Role_Name = db.Column(db.String(50), db.ForeignKey('Role.Role_Name'), nullable=False)
+    publish_Date = db.Column(db.Date, nullable=False)
+    Closing_date = db.Column(db.Date, nullable=False)
+
+    def __init__(self, Role_Name, publish_Date, Closing_date):
+        self.Role_Name = Role_Name
+        self.publish_Date = publish_Date
+        self.Closing_date = Closing_date
+
+    def json(self):
+        return {
+            "JobList_ID": self.JobList_ID,
+            "Role_Name": self.Role_Name,
+            "publish_Date": self.publish_Date.strftime('%Y-%m-%d'),
+            "Closing_date": self.Closing_date.strftime('%Y-%m-%d')
+        }
+
+
+#Get roles in company
+@app.route("/roles")
+def get_all_roles():
+    rolelist = Role.query.all()
+    if len(rolelist):
+        return jsonify(
+            {
+                "code": 200,
+                "data": {
+                    "roles": [role.json() for role in rolelist]
+                }
+            }
+        )
+    return jsonify(
+        {
+            "code": 404,
+            "message": "There are no roles available."
+        }
+    ), 404
+
+#Get all Job Listings
+@app.route("/joblistings")
+def get_all_joblistings():
+    joblistings = JobListing.query.all()
+    if len(joblistings):
+        return jsonify(
+            {
+                "code": 200,
+                "data": {
+                    "joblistings": [joblisting.json() for joblisting in joblistings]
+                }
+            }
+        )
+    return jsonify(
+        {
+            "code": 404,
+            "message": "There are no job listings that are currently open."
+        }
+    ), 404
+
+
+
 
 @app.route("/apply_role", methods=['POST'])
 def apply_role():
--- Frontend/js/joblisting.js
+++ Frontend/js/joblisting.js
@@ -19,3 +19,60 @@ form.addEventListener("submit", function (event) {
 
     }); 
 });
+
+
+
+
+const get_roles_URL = "http://localhost:5000/roles";
+const get_joblistings_URL = "http://localhost:5100/joblistings";
+
+// Vue
+const jobsPage = Vue.createApp({
+  data() {
+      return {
+          jobListings: [],
+          roles: [],
+          accessRight: 0,
+      };
+  },
+
+  mounted() {
+      console.log("-------In user mounted------");
+      // retrieve all job listings
+      axios
+          .get(get_joblistings_URL)
+          .then((response) => {
+              console.log("job listings loaded");
+              console.log(response.data[0]);
+              this.jobListings = response.data["data"]["joblistings"];
+
+          })
+          .catch((error) => {
+              console.log(error);
+          });
+    
+  },
+
+  methods: {
+
+    getAllRoles () {
+      // on Vue instance created, load the book list
+      axios
+          .get(get_roles_URL)
+          .then((response) => {
+                  console.log(response);
+                  this.roles = response.data["data"]["roles"];
+              })
+              .catch(error => {
+                  // Errors when calling the service; such as network error, 
+                  // service offline, etc
+                  console.log(error);
+
+              });
+
+  },
+
+  },
+});
+
+const vm = jobsPage.mount("#jobsPage");
