fb0ea89300c5c1bf439541af705afcb476ea64b9
Seekay <gohcheekiat@gmail.com>
2023-09-18 13:52:21 +0800

    update job listings page

--- Backend/app.py
+++ Backend/app.py
@@ -8,3 +8,29 @@ import amqp_setup
 import pika
 import json
 # --------------- Import for Notification Service (End) ---------------
+
+# -------------- Import for Job Listing /createlisting (START) --------------
+from datetime import datetime
+# -------------- Import for Job Listing /createlisting (END) --------------
+app = Flask(__name__)
+
+
+# Process job listing creation 
+@app.route('/createlisting', methods=['POST'])
+def process_data():
+    data = request.get_json()
+    roletitle = data["roletitle"] 
+    closingdate = data["closingdate"]
+    date = datetime.now()
+    if (closingdate < date): 
+        return "Error, closing date cannot be the day before "
+    # elif (roletitle in (db) and closing date in (db)): 
+    #     return "Error, cannot have duplicate listings"
+ 
+    # Return a response, for example, a confirmation message
+    else: 
+        return jsonify({"message": "Data received and processed successfully"})
+
+
+if __name__ == "__main__": 
+    app.run(host='0.0.0.0', port=5000, debug=True)
---
+++ Frontend/js/joblisting.js
@@ -0,0 +1,21 @@
+const form = document.querySelector("form");
+const roletitle = document.getElementById("roletitle").value;
+const closingdate = document.getElementById("closingdate").value;
+
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault(); // Prevent the default form submission behavior
+  
+  // Send the data to the Flask backend using Axios
+  axios.post('https://127.0.0.1:5000/createlisting',  {,
+    "roletitle":  roletitle,
+    "closingdate" : closingdate, 
+    })
+    .then((response) => {
+      console.log("Data sent successfully:", response.data);
+    })
+    .catch((error) => {
+      console.error("Error sending data:", error);
+
+    });
+});
