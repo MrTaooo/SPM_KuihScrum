37c916f436264a67153817a673c0eac2fc41cd83
junyiiii <junyi.low.2021@scis.smu.edu.sg>
2023-09-26 14:00:01 +0800

    added an ID for close modal and finished test case for create listings

--- tests/unit/CofRoleListings_pos.py
+++ tests/unit/CofRoleListings_pos.py
@@ -1,10 +1,13 @@
 # import webdriver
 from selenium import webdriver
-import time
 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+import time
 
 # create webdriver object
 driver = webdriver.Chrome()
@@ -22,10 +25,10 @@ element = driver.find_element(By.XPATH, "//button[@class='btn btn-dark']")
 element.click()
 time.sleep(1)
 dropdown = Select(driver.find_element(By.ID, "roleTitle")) 
-roleTitle = "Sales Representative"
+roleTitle = "Software Developer"
 dropdown.select_by_visible_text(roleTitle)
 
-# Locate the input field that opens the date picker and enter in a date
+# enter date
 year = "2023"
 month = "09"
 day = "27"
@@ -46,24 +49,28 @@ submit = driver.find_element(By.ID, "jobCreationButton")
 submit.click()
 time.sleep(3)
 
+# wait for modal to appear
+wait = WebDriverWait(driver, 10)
+wait.until(EC.visibility_of_element_located((By.ID, "closeModal")))
+
 # close modal
-close = driver.find_element(By.XPATH, "//button[@data-bs-dismiss='modal']")
+close = driver.find_element(By.ID, "closeModal")
 close.click()
 
 expected_closing_dates[roleTitle] = date # add to dictionary
 
-# Locate all job title elements and closing date elements
+# find all job title and closing dates
 job_title_elements = driver.find_elements(By.CSS_SELECTOR, ".card-title.m-2")
 closing_date_elements = driver.find_elements(By.XPATH, "//div[contains(@class, 'text-center')]/h5[contains(text(), 'Closing Date:')]")
 
-# Ensure there are job titles and closing dates
+# job titles and closing date elements
 if job_title_elements and closing_date_elements:
-    # Get the latest job title and closing date
+    # latest job title and closing date
     latest_job_title = job_title_elements[-1].text
     latest_closing_date_element = closing_date_elements[-1]
     latest_closing_date_text = latest_closing_date_element.text.split(":")[1].strip()
 
-    # Check if the latest closing date matches the expected date
+    # check if the latest closing date matches the expected date
     if latest_job_title in expected_closing_dates:
         expected_date = expected_closing_dates[latest_job_title]
         if latest_closing_date_text == expected_date:
