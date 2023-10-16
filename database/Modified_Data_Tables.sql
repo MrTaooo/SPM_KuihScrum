31425ad5ff146ebe8d3d2b5e3479ad4a297965de
MrTaooo <95959934+MrTaooo@users.noreply.github.com>
2023-10-16 11:04:46 +0800

    (With Error) Modified Data Table

---
+++ database/Modified_Data_Tables.sql
@@ -0,0 +1,70 @@
+DROP SCHEMA IF EXISTS SPM_KUIH;
+
+CREATE SCHEMA SPM_KUIH;
+USE SPM_KUIH;
+
+DROP TABLE IF EXISTS `AccessRights`;
+DROP TABLE IF EXISTS `Staff`;
+DROP TABLE IF EXISTS `Role`;
+DROP TABLE IF EXISTS `Skill`;
+DROP TABLE IF EXISTS `Role_Skill`;
+DROP TABLE IF EXISTS `Staff_Skill`;
+DROP TABLE IF EXISTS `Job_Listing`;
+DROP TABLE IF EXISTS `Job_Application`;  
+
+CREATE TABLE AccessRights (
+    Access_ID int PRIMARY KEY,
+    Access_Control_Name varchar(50) NOT NULL
+);
+
+CREATE TABLE Staff (
+    Staff_ID int PRIMARY KEY,
+    Staff_FName Varchar(50) NOT NULL,
+    Staff_LName Varchar(50) NOT NULL,
+    Dept Varchar(50) NOT NULL,
+    Country Varchar(50) NOT NULL,
+    Email Varchar(50) NOT NULL,
+    Access_Rights int,
+    FOREIGN KEY (Access_Rights) REFERENCES AccessRights(Access_ID)
+);
+
+CREATE TABLE Role (
+	Role_Name Varchar(20) Primary Key,
+    Role_Desc Longtext Not Null
+);
+
+CREATE TABLE Skill (
+	Skill_Name Varchar(50) Primary Key,
+    Skill_Desc Longtext Not Null
+);
+
+CREATE TABLE Role_Skill (
+    Role_Name Varchar(20),
+    Skill_Name Varchar(50),
+    PRIMARY KEY (Role_Name, Skill_Name),
+    FOREIGN KEY (Role_Name) REFERENCES Role(Role_Name),
+    FOREIGN KEY (Skill_Name) REFERENCES Skill(Skill_Name)
+);
+
+CREATE TABLE Staff_Skill (
+    Staff_ID int,
+    Skill_Name Varchar(50),
+    FOREIGN KEY (Staff_ID) REFERENCES Staff(Staff_ID),
+    FOREIGN KEY (Skill_Name) REFERENCES Role_Skill(Skill_Name)
+);
+
+CREATE TABLE Job_Listing (
+    JobList_ID INT PRIMARY KEY AUTO_INCREMENT,
+    Role_Name Varchar(50),
+    publish_Date DATE NOT NULL,
+    Closing_date DATE NOT NULL,
+    FOREIGN KEY (Role_Name) REFERENCES Role(Role_Name)
+);
+
+CREATE TABLE Job_Application (
+    JobList_ID INT NOT NULL,
+    Staff_ID INT NOT NULL,
+    PRIMARY KEY (JobList_ID, staff_id),
+    FOREIGN KEY (JobList_ID) REFERENCES Job_Listing(JobList_ID),
+    FOREIGN KEY (Staff_ID) REFERENCES Staff(Staff_ID)
+);
