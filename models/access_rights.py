be525391bf8ec43e570905c9d6397d8f849a7a3f
MrTaooo <95959934+MrTaooo@users.noreply.github.com>
2023-10-16 15:32:27 +0800

    Data Table Fixed!

--- models/access_rights.py
+++ models/access_rights.py
@@ -5,15 +5,15 @@ class AccessRights(db.Model):
     __tablename__ = 'AccessRights'
 
     Access_ID = db.Column(db.Integer, primary_key=True)
-    Access_type = db.Column(db.String(50), nullable=False)
+    Access_Control_Name = db.Column(db.String(50), nullable=False)
 
-    def __init__(self, Access_ID, Access_type):
+    def __init__(self, Access_ID, Access_Control_Name):
         self.Access_ID = Access_ID
-        self.Access_type = Access_type
+        self.Access_Control_Name = Access_Control_Name
     
     def json(self):
         return {
             "Access_ID": self.Access_ID,
-            "Access_type": self.Access_type
+            "Access_Control_Name": self.Access_Control_Name
         }
     
