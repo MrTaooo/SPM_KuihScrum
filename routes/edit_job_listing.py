b631a63a13fda6e1d564a16edb01ca5759af1a68
Seekay <gohcheekiat@gmail.com>
2023-10-17 18:35:21 +0800

    fixed error on editListing

--- routes/edit_job_listing.py
+++ routes/edit_job_listing.py
@@ -5,8 +5,10 @@ from datetime import datetime
 
 def edit_listing():
     data = request.get_json()
+    print(data)
     listing_id = data["id"]
     new_closingDate = data["closingDate"]
+    roleName = data["roleName"]
     current_date = datetime.now().strftime("%Y-%m-%d")
 
     # check if the new closing date is empty
@@ -28,7 +30,22 @@ def edit_listing():
             "code": 404,
             "message": "Error, listing not found"
         })
-
+    
+     # retrieve duplicate role listing (exact role name, publish date is before closing date, closing date is after today)
+    overlapping_listings = db.session.query(JobListing).filter(
+        JobListing.Role_Name == roleName,
+        JobListing.publish_Date <= new_closingDate,
+        JobListing.Closing_date >= current_date,
+        JobListing.JobList_ID != listing_id
+    ).all()
+    print(overlapping_listings)
+    # Check if the role listing for the role exists in the database
+    if overlapping_listings:
+        return jsonify({
+            "code": 409,
+            "message": "Error, cannot have duplicate listings"
+        })
+   
     listing.Closing_date = new_closingDate
     db.session.commit()
 
@@ -36,3 +53,5 @@ def edit_listing():
         "code": 200,
         "message": "Listing updated successfully"
     })
+
+    
